2bef6f0b7e2f053924a8e7752b9bdedf9b5f0616
AkiBoba <lettervl@ya.ru>
2022-03-17 16:07:43 +0300

    2. Связь слоев через Spring DI. [#504856]

--- src/main/java/ru/job4j/dream/store/PostStore.java
+++ src/main/java/ru/job4j/dream/store/PostStore.java
@@ -1,16 +1,20 @@
 package ru.job4j.dream.store;
 
 
+import org.springframework.stereotype.Repository;
 import ru.job4j.dream.model.Post;
 
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
+@Repository
 public class PostStore {
 
-    private static final PostStore INST = new PostStore();
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+
+    private final AtomicInteger ids = new AtomicInteger(4);
 
     private PostStore() {
         posts.put(1, new Post(1, "Junior Java Job",
@@ -23,16 +27,13 @@ public class PostStore {
                 "Senior Java", "01-03-2022"));
     }
 
-    public static PostStore instOf() {
-        return INST;
-    }
-
     public Collection<Post> findAll() {
         return posts.values();
     }
 
     public void add(Post post) {
-        this.posts.put(post.getId(), post);
+        post.setId(ids.incrementAndGet());
+        posts.put(post.getId(), post);
     }
 
     public Post findById(int id) {
