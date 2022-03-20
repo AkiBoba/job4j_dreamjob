c02d0fbbcf72e55177cc4e621461f3dfd3157075
Vladimir Likhachev <lettervl@ya.ru>
2022-03-20 10:18:17 +0300

    5. Создания и редактирования кандидатов. [#504858 #276405]

--- src/main/java/ru/job4j/dreamjob/controller/PostController.java
+++ src/main/java/ru/job4j/dreamjob/controller/PostController.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class PostController {
     private final PostService postService;
+
     public PostController(PostService postService) {
         this.postService = postService;
     }
